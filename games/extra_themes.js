5bb321d93c8f2a4e16aa1e7fc4136b0e985bfcfb
mattOd80 <mod80.projects@gmail.com>
2023-08-31 23:19:18 -0400

    wip

---
+++ games/extra_themes.js
@@ -0,0 +1,17 @@
+const ExtraThemes = {
+    neon: { gridColor: '#00FFFF', backgroundColor: 'black', atomColor: '#39FF14', atom3D: true, uiBackground: '#000', uiText: '#0ff', uiHighlight: '#f0f' },
+    retro: { gridColor: 'brown', backgroundColor: 'beige', atomColor: 'darkbrown', atom3D: false, uiBackground: '#f5deb3', uiText: '#8b4513', uiHighlight: '#a0522d' },
+    cyberpunk: { gridColor: '#FF4081', backgroundColor: '#1A1A1A', atomColor: '#BB86FC', atom3D: true, uiBackground: '#121212', uiText: '#03DAC6', uiHighlight: '#6200EA' },
+    sunrise: { gridColor: '#FFD700', backgroundColor: '#FF6347', atomColor: '#FF4500', atom3D: false, uiBackground: '#FFA500', uiText: '#FFDAB9', uiHighlight: '#FFB6C1' },
+    forest: { gridColor: '#228B22', backgroundColor: '#006400', atomColor: '#8FBC8F', atom3D: true, uiBackground: '#2E8B57', uiText: '#ADFF2F', uiHighlight: '#32CD32' },
+    lateNightCodeBinge: { gridColor: '#2E2E2E', backgroundColor: '#1C1C1C', atomColor: '#00FF00', atom3D: true, uiBackground: '#121212', uiText: '#00FF00', uiHighlight: '#0F0F0F' },
+    tooTiredForThis: { gridColor: '#A9A9A9', backgroundColor: '#696969', atomColor: '#808080', atom3D: false, uiBackground: '#C0C0C0', uiText: '#D3D3D3', uiHighlight: '#778899' },
+    hellYeaInTheFlow: { gridColor: '#FFD700', backgroundColor: '#FF4500', atomColor: '#FF6347', atom3D: true, uiBackground: '#FFA500', uiText: '#FFDAB9', uiHighlight: '#FFB6C1' },
+    matrix: { gridColor: '#00FF00', backgroundColor: '#000000', atomColor: '#00FF00', atom3D: true, uiBackground: '#000000', uiText: '#00FF00', uiHighlight: '#008000' },
+    tron: { gridColor: '#1A1AFA', backgroundColor: '#000000', atomColor: '#1A1AFA', atom3D: true, uiBackground: '#000000', uiText: '#1A1AFA', uiHighlight: '#0F0F8F' },
+    bladeRunner: { gridColor: '#FF4500', backgroundColor: '#2F4F4F', atomColor: '#FF6347', atom3D: true, uiBackground: '#696969', uiText: '#FFA07A', uiHighlight: '#FF7F50' },
+    cyberneticDream: { gridColor: '#FF69B4', backgroundColor: '#8A2BE2', atomColor: '#DDA0DD', atom3D: false, uiBackground: '#9932CC', uiText: '#9400D3', uiHighlight: '#BA55D3' },
+    starshipConsole: { gridColor: '#B22222', backgroundColor: '#8B0000', atomColor: '#FF0000', atom3D: true, uiBackground: '#A52A2A', uiText: '#FA8072', uiHighlight: '#E9967A' },
+    alienInvasion: { gridColor: '#7FFF00', backgroundColor: '#32CD32', atomColor: '#ADFF2F', atom3D: false, uiBackground: '#6B8E23', uiText: '#556B2F', uiHighlight: '#9ACD32' },
+    holographicUI: { gridColor: '#00CED1', backgroundColor: '#20B2AA', atomColor: '#40E0D0', atom3D: true, uiBackground: '#5F9EA0', uiText: '#B0E0E6', uiHighlight: '#48D1CC' }
+}
